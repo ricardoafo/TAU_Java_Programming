b5de792ba20e987783ff1672f5eb879661fb0a5f
Ricardo Afonso <r.afonsomontero@gmail.com>
2023-06-19 16:27:15 -0300

    Added new classes

---
+++ src/main/java/chapter6/HomeAreaCalculator.java
@@ -0,0 +1,19 @@
+package chapter6;
+
+public class HomeAreaCalculator {
+
+    public static void main(String[] args) {
+        // Create instance of Rectangle class
+        Rectangle room1 = new Rectangle();
+        room1.setWidth(25);
+        room1.setLength(50);
+        double areaOfRoom1 = room1.calculateArea();
+
+        Rectangle room2 = new Rectangle(30, 75);
+        double areaOfRoom2 = room2.calculateArea();
+
+        double totalArea = areaOfRoom1 + areaOfRoom2;
+
+        System.out.println("Area of both rooms: " + totalArea);
+    }
+}
